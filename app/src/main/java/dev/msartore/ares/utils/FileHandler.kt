26bc503f8435dee8a692064baea7c0b90949ba85
Massimiliano Sartore <massimiliano.sartore@protonmail.com>
2022-09-15 12:41:50 +0200

    Add more file format icons

--- app/src/main/java/dev/msartore/ares/utils/FileHandler.kt
+++ app/src/main/java/dev/msartore/ares/utils/FileHandler.kt
@@ -6,6 +6,7 @@ import android.net.Uri
 import android.provider.OpenableColumns
 import dev.msartore.ares.R
 import dev.msartore.ares.models.FileData
+import java.util.*
 
 fun ContentResolver.extractFileInformation(uri: Uri): FileData? {
 
@@ -29,11 +30,13 @@ fun ContentResolver.extractFileInformation(uri: Uri): FileData? {
             fileData.apply {
                 size = displaySize
                 name = displayName
-                fileType = fileType(displayName)
+                fileType = fileType(displayName.lowercase(Locale.ROOT))
                 icon = when (fileType) {
                     FileType.VIDEO -> R.drawable.video_file_24px
                     FileType.IMAGE -> R.drawable.image_24px
                     FileType.DOCUMENT -> R.drawable.description_24px
+                    FileType.ZIP -> R.drawable.folder_zip_24px
+                    FileType.APK -> R.drawable.apk_document_24px
                     else -> R.drawable.draft_24px
                 }
             }
@@ -45,16 +48,23 @@ fun ContentResolver.extractFileInformation(uri: Uri): FileData? {
 
 fun fileType(name: String): FileType {
     return when {
-        listOf("mp4").any { name.contains(it) } -> FileType.VIDEO
-        listOf("jpg", "png").any { name.contains(it) } -> FileType.IMAGE
-        listOf("pdf", "txt").any { name.contains(it) } -> FileType.DOCUMENT
+        name.contains(listOf("mp4", "mkv", "webm", "3gp")) -> FileType.VIDEO
+        name.contains(listOf("jpg", "png", "raw", "psd", "bmp", "jpeg", "gif", "dng", "tiff")) -> FileType.IMAGE
+        name.contains(listOf("pdf", "txt", "docs")) -> FileType.DOCUMENT
+        name.contains(listOf("apk")) -> FileType.APK
+        name.contains(listOf("zip")) -> FileType.ZIP
         else -> FileType.UNKNOWN
     }
 }
+
+fun String.contains(collection: Collection<String>) =
+    collection.any { this.contains(it) }
 
 enum class FileType {
     VIDEO,
     IMAGE,
     DOCUMENT,
+    ZIP,
+    APK,
     UNKNOWN
 }
